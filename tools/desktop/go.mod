f20157e69a1d8f01a298edeb0b76ae29c5fef464
Kyle Unverferth <kyle@kyleu.com>
2021-07-15 14:42:38 -0600

    Desktop app

---
+++ tools/desktop/go.mod
@@ -0,0 +1,10 @@
+module github.com/kyleu/projectforge/tools/desktop
+
+go 1.15
+
+require (
+	github.com/kyleu/projectforge v0.0.0
+	github.com/webview/webview v0.0.0-20210330151455-f540d88dde4e
+)
+
+replace github.com/kyleu/projectforge => ../..
---
+++ tools/desktop/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/kyleu/projectforge/app/cmd"
+	"github.com/kyleu/projectforge/app/util"
+	"github.com/webview/webview"
+)
+
+func main() {
+	if err := run(true); err != nil {
+		panic(err)
+	}
+}
+
+func run(debug bool) error {
+	port, err := cmd.Lib()
+	if err != nil {
+		return err
+	}
+	loadWebview(debug, int(port))
+	return nil
+}
+
+func loadWebview(debug bool, port int) {
+	w := webview.New(debug)
+	defer w.Destroy()
+
+	w.SetTitle(util.AppName)
+	w.SetSize(1280, 720, webview.HintNone)
+	w.Navigate(fmt.Sprintf("http://localhost:%d", port))
+
+	w.Run()
+}
