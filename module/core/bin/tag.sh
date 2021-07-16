c25bcb4dcdf875455ca2c61763024b985dc77fc6
Kyle Unverferth <kyle@kyleu.com>
2021-07-16 09:42:36 -0600

    v0.0.1

---
+++ module/core/bin/tag.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+## Tags the git repo
+
+set -euo pipefail
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $dir/..
+
+TGT=$1
+[ "$TGT" ] || (echo "must provide one argument, like \"0.0.1\"" && exit)
+
+find . -type f -name "main.go" -print0 | xargs -0 sed -i '' -e "s/[0-9]*[0-9]\.[0-9]*[0-9]\.[0-9]*[0-9]/${TGT}/g"
+find . -type f -name ".projectforge.json" -print0 | xargs -0 sed -i '' -e "s/[0-9]*[0-9]\.[0-9]*[0-9]\.[0-9]*[0-9]/${TGT}/g"
+
+make build
+
+git add .
+git commit -m "v${TGT}" || true
+
+git tag "v${TGT}"
+
+git push
+git push --tags
