0da6b166543350b6c3c1e7778c5684d1ac13c721
Kyle Unverferth <kyle@kyleu.com>
2021-08-28 09:23:57 -0600

    v0.0.9

--- module/core/bin/tag.sh
+++ module/core/bin/tag.sh
@@ -9,8 +9,12 @@ cd $dir/..
 TGT=$1
 [ "$TGT" ] || (echo "must provide one argument, like \"0.0.1\"" && exit)
 
-find . -type f -name "main.go" -print0 | xargs -0 sed -i '' -e "s/version = \\\"[0-9]*[0-9]\.[0-9]*[0-9]\.[0-9]*[0-9]\\\"/version = \"${TGT}\"/g"
-find . -type f -name ".projectforge.json" -print0 | xargs -0 sed -i '' -e "s/\\\"version\\\": \\\"[0-9]*[0-9]\.[0-9]*[0-9]\.[0-9]*[0-9]\\\"/\"version\": \"${TGT}\"/g"
+find . -type f -name "main.go" -print0 | xargs -0 sed -i '' -e "s/version = \\\"[v]*[0-9]*[0-9]\.[0-9]*[0-9]\.[0-9]*[0-9]\\\"/version = \"${TGT}\"/g"
+find . -type f -name ".projectforge.json" -print0 | xargs -0 sed -i '' -e "s/\\\"version\\\": \\\"[v]*[0-9]*[0-9]\.[0-9]*[0-9]\.[0-9]*[0-9]\\\"/\"version\": \"${TGT}\"/g"
+{{{if .Build.SkipNotarize}}}{{{else}}}
+cd tools/notarize
+find . -type f -name "gon.*.hcl" -print0 | xargs -0 sed -i '' -e "s/_[v]*[0-9]*[0-9]\.[0-9]*[0-9]\.[0-9]*[0-9]_/_${TGT}_/g"
+cd ../..{{{ end }}}
 
 make build
 
