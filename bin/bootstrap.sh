e42caac15c56afe9377f07d535c58b056fead420
Kyle Unverferth <kyle@kyleu.com>
2021-09-04 12:00:12 -0600

    Go 1.17

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -8,7 +8,6 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $dir/..
 
 go get -u github.com/cosmtrek/air
-go get -u github.com/pyros2097/go-embed
 go get -u github.com/valyala/quicktemplate
 go get -u github.com/valyala/quicktemplate/qtc
 go mod download
