4615ad62ef3aa4f697682c2a08c1f51d966e1887
Kyle Unverferth <kyle@kyleu.com>
2022-01-08 05:44:31 -0700

    v0.1.29

--- module/core/bin/tag.sh
+++ module/core/bin/tag.sh
@@ -21,10 +21,12 @@ sed -i.bak -e "s/version = \\\"[v]*[0-9]*[0-9]\.[0-9]*[0-9]\.[0-9]*[0-9]\\\"/ver
 rm -f "./main.go.bak"{{{ if .UsesLib }}}
 sed -i.bak -e "s/Version: \\\"[v]*[0-9]*[0-9]\.[0-9]*[0-9]\.[0-9]*[0-9]\\\"/Version: \"${TGT}\"/g" ./app/cmd/lib.go
 rm -f ./app/cmd/lib.go.bak{{{ end }}}{{{ if .Build.Notarize }}}
-find . -type f -name "gon.*.hcl" -print0 | xargs -0 sed -i.bak -e "s/\\_[v]*[0-9]*[0-9]\.[0-9]*[0-9]\.[0-9]*[0-9]_/_${TGT}\\_/g"
-rm ./tools/notarize/*.hcl.bak{{{ end }}}
+sed -i.bak -e "s/\\_[v]*[0-9]*[0-9]\.[0-9]*[0-9]\.[0-9]*[0-9]_/_${TGT}\\_/g" ./tools/notarize/gon.amd64.hcl
+rm -f "./tools/notarize/gon.amd64.hcl.bak"
+sed -i.bak -e "s/\\_[v]*[0-9]*[0-9]\.[0-9]*[0-9]\.[0-9]*[0-9]_/_${TGT}\\_/g" ./tools/notarize/gon.arm64.hcl
+rm -f "./tools/notarize/gon.arm64.hcl.bak"{{{ end }}}
 sed -i.bak -e "s/\\\"version\\\": \\\"[v]*[0-9]*[0-9]\.[0-9]*[0-9]\.[0-9]*[0-9]\\\"/\"version\": \"${TGT}\"/g" ./.projectforge.json
-rm -f "./.projectforge.json.bak"
+rm -f "./.projectforge.json.bak" || true
 
 make build
 
