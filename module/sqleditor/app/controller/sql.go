30c557d69ec149c8fe19e95e85e60ed0ea78f7cc
Kyle Unverferth <kyle@kyleu.com>
2022-01-08 05:38:04 -0700

    SQL editor

---
+++ module/sqleditor/app/controller/sql.go
@@ -0,0 +1,71 @@
+package controller
+
+import (
+	"strings"
+
+	"github.com/pkg/errors"
+	"github.com/valyala/fasthttp"
+
+	"{{{ .Package }}}/app"
+	"{{{ .Package }}}/app/controller/cutil"
+	"{{{ .Package }}}/app/util"
+	"{{{ .Package }}}/views/vsql"
+)
+
+func SQLEditor(rc *fasthttp.RequestCtx) {
+	act("sql.editor", rc, func(as *app.State, ps *cutil.PageState) (string, error) {
+		ps.Title = "SQL Editor"
+		ps.Data = "Post to this action with [sql] in the body"
+		return render(rc, as, &vsql.SQLEditor{}, ps, "sql")
+	})
+}
+
+func SQLRun(rc *fasthttp.RequestCtx) {
+	act("sql.run", rc, func(as *app.State, ps *cutil.PageState) (string, error) {
+		sql := "select 1;"
+		commit := false
+		meth := string(rc.Method())
+		if strings.EqualFold(meth, "post") {
+			f := rc.PostArgs()
+			sql = string(f.Peek("sql"))
+			c := string(f.Peek("commit"))
+			commit = c == "true"
+		}
+
+		tx, err := as.DB.StartTransaction()
+		if err != nil {
+			return "", errors.Wrap(err, "unable to start transaction")
+		}
+
+		var columns []string
+		results := [][]interface{}{}
+
+		start := util.TimerStart()
+		result, err := as.DB.Query(ps.Context, sql, tx)
+		elapsed := util.TimerEnd(start)
+
+		for result.Next() {
+			if columns == nil {
+				columns, _ = result.Columns()
+			}
+			row, err := result.SliceScan()
+			if err != nil {
+				return "", errors.Wrap(err, "unable to read row")
+			}
+			results = append(results, row)
+		}
+
+		if commit {
+			err = tx.Commit()
+			if err != nil {
+				return "", errors.Wrap(err, "unable to commit transaction")
+			}
+		} else {
+			_ = tx.Rollback()
+		}
+
+		ps.Title = "SQL Results"
+		ps.Data = results
+		return render(rc, as, &vsql.SQLEditor{SQL: sql, Columns: columns, Results: results, Timing: elapsed, Commit: commit}, ps, "sql")
+	})
+}
---
+++ module/sqleditor/doc/module/sqleditor.md
@@ -0,0 +1,13 @@
+# [mysql]
+
+This is a module for [Project Forge](https://projectforge.dev). It provides an API for accessing MySQL databases.
+
+https://github.com/kyleu/projectforge/tree/master/module/mysql
+
+### License
+
+Licensed under [CC0](https://creativecommons.org/share-your-work/public-domain/cc0)
+
+### Usage
+
+There's a SQL editor. Good luck!
