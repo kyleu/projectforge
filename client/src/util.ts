9cf3d95789d6eb8b8b66dfc9b2673e59a2d6e3bf
Kyle Unverferth <kyle@kyleu.com>
2023-02-07 12:09:26 -0700

    Temporarily disabling android build

--- client/src/util.ts
+++ client/src/util.ts
@@ -2,12 +2,25 @@
 export const appKey = "projectforge";
 export const appName = "Project Forge";
 
-export function svgRef(key: string, size: number): string {
+export function svgRef(key: string, size?: number, cls?: string): string {
+  if (!size) {
+    size = 18;
+  }
+  if (cls) {
+    return `<svg class="${cls}" style="width: ${size}px; height: ${size}px;"><use xlink:href="#svg-${key}"></use></svg>`;
+  }
   return `<svg class="icon" style="width: ${size}px; height: ${size}px;"><use xlink:href="#svg-${key}"></use></svg>`;
 }
 
-export function svg(key: string, cls?: string) {
-  return {
-    "__html": `<svg class="${cls || ""}" style="width: 18px; height: 18px;"><use href="#svg-${key}"></use></svg>`
+export function svg(key: string, size?: number, cls?: string) {
+  return {"__html": svgRef(key, size, cls)};
+}
+
+export function expandCollapse(extra?: string) {
+  if (!extra) {
+    extra = "";
   }
+  let e = svgRef("right", 15, "expand");
+  let c = svgRef("down", 15, "collapse");
+  return {"__html": e + c + extra};
 }
