46f62273a7aaeda92dba19825413329f7285fe7d
Kyle Unverferth <kyle@kyleu.com>
2023-10-29 17:29:18 -0600

    Removing misguided "vmbuild" module

--- module/android/tools/android/readme.md
+++ module/android/tools/android/readme.md
@@ -1,3 +1,7 @@
 # {{{ .Name }}} Android Project
 
 This folder contains the template for the Android build of {{{ .Name }}}.
+
+```shell
+brew install ant maven gradle android-sdk android-ndk
+```
--- module/vmbuild/tools/vmbuild/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $dir
-
-echo "running [build-machine]...";
-tart run --dir=data:./data --dir={{{ .Exec }}}:../.. {{{ .Exec }}}-build-machine
-
