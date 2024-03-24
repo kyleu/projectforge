f15942a85d36cfe6e6e9496d057ebb5ff64dc741
Kyle Unverferth <kyle@kyleu.com>
2024-03-24 16:56:08 -0600

    Rebuilding notarization framework

--- module/core/bin/tag.sh
+++ module/core/bin/tag.sh
@@ -49,13 +49,7 @@ if [[ $TGT =~ $pat ]]; then
   sed -i.bak -e "s/version = \\\"[v]*[0-9]*[0-9]\.[0-9]*[0-9]\.[0-9]*[0-9]\\\"/version = \"${TGT}\"/g" ./main.go
   rm -f "./main.go.bak"{{{ if .UsesLib }}}
   sed -i.bak -e "s/Version: \\\"[v]*[0-9]*[0-9]\.[0-9]*[0-9]\.[0-9]*[0-9]\\\"/Version: \"${TGT}\"/g" ./app/cmd/lib.go
-  rm -f ./app/cmd/lib.go.bak{{{ end }}}{{{ if .BuildNotarize }}}
-  sed -i.bak -e "s/\\_[v]*[0-9]*[0-9]\.[0-9]*[0-9]\.[0-9]*[0-9]_/_${TGT}\\_/g" ./tools/notarize/gon.amd64.hcl
-  rm -f "./tools/notarize/gon.amd64.hcl.bak"
-  sed -i.bak -e "s/\\_[v]*[0-9]*[0-9]\.[0-9]*[0-9]\.[0-9]*[0-9]_/_${TGT}\\_/g" ./tools/notarize/gon.arm64.hcl
-  rm -f "./tools/notarize/gon.arm64.hcl.bak"
-  sed -i.bak -e "s/\\_[v]*[0-9]*[0-9]\.[0-9]*[0-9]\.[0-9]*[0-9]_/_${TGT}\\_/g" ./tools/notarize/gon.all.hcl
-  rm -f "./tools/notarize/gon.all.hcl.bak"{{{ end }}}
+  rm -f ./app/cmd/lib.go.bak{{{ end }}}
   sed -i.bak -e "s/\\\"version\\\": \\\"[v]*[0-9]*[0-9]\.[0-9]*[0-9]\.[0-9]*[0-9]\\\"/\"version\": \"${TGT}\"/g" ./.projectforge/project.json
   rm -f "./.projectforge/project.json.bak"
 fi
