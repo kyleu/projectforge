822f0d58fc6494b69d9f25dd3512d3030cbe4a8b
Kyle Unverferth <kyle@kyleu.com>
2024-05-06 11:21:54 -0600

    Database updates

--- module/database/app/lib/database/engine.go
+++ module/database/app/lib/database/engine.go
@@ -2,6 +2,8 @@ package database
 
 import (
 	"fmt"
+
+	"github.com/samber/lo"
 )
 
 type DBType struct {
@@ -25,4 +27,17 @@ func (t *DBType) PlaceholderFor(idx int) string {
 
 func (t *DBType) Quoted(s string) string {
 	return fmt.Sprintf("%s%s%s", t.Quote, s, t.Quote)
+}
+
+var AllTypes = []*DBType{
+{{{ if .MySQL }}}	TypeMySQL,
+{{{ end }}}{{{ if .PostgreSQL }}}	TypePostgres,
+{{{ end }}}{{{ if .SQLite }}}	TypeSQLite,
+{{{ end }}}{{{ if .SQLServer }}}	TypeSQLServer,
+{{{ end }}}}
+
+func TypeByKey(key string) *DBType {
+	return lo.FindOrElse(AllTypes, nil, func(x *DBType) bool {
+		return x.Key == key
+	})
 }
