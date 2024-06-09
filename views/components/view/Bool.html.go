213f312e28eed603b4698404501dce71693fbc41
Kyle Unverferth <kyle@kyleu.com>
2024-06-08 20:19:43 -0600

    Bool icon improvements

--- views/components/view/Bool.html.go
+++ views/components/view/Bool.html.go
@@ -8,106 +8,138 @@ package view
 
 //line views/components/view/Bool.html:2
 import (
+	"fmt"
+
 	"projectforge.dev/projectforge/app/controller/cutil"
+	"projectforge.dev/projectforge/app/util"
 	"projectforge.dev/projectforge/views/components"
 )
 
-//line views/components/view/Bool.html:7
+//line views/components/view/Bool.html:10
 import (
 	qtio422016 "io"
 
 	qt422016 "github.com/valyala/quicktemplate"
 )
 
-//line views/components/view/Bool.html:7
+//line views/components/view/Bool.html:10
 var (
 	_ = qtio422016.Copy
 	_ = qt422016.AcquireByteBuffer
 )
 
-//line views/components/view/Bool.html:7
+//line views/components/view/Bool.html:10
 func StreamBool(qw422016 *qt422016.Writer, b bool) {
-//line views/components/view/Bool.html:8
-	if b {
-//line views/components/view/Bool.html:8
-		qw422016.N().S(`true`)
-//line views/components/view/Bool.html:8
-	} else {
-//line views/components/view/Bool.html:8
-		qw422016.N().S(`false`)
-//line views/components/view/Bool.html:8
-	}
-//line views/components/view/Bool.html:9
-}
-
-//line views/components/view/Bool.html:9
-func WriteBool(qq422016 qtio422016.Writer, b bool) {
-//line views/components/view/Bool.html:9
-	qw422016 := qt422016.AcquireWriter(qq422016)
-//line views/components/view/Bool.html:9
-	StreamBool(qw422016, b)
-//line views/components/view/Bool.html:9
-	qt422016.ReleaseWriter(qw422016)
-//line views/components/view/Bool.html:9
-}
-
-//line views/components/view/Bool.html:9
-func Bool(b bool) string {
-//line views/components/view/Bool.html:9
-	qb422016 := qt422016.AcquireByteBuffer()
-//line views/components/view/Bool.html:9
-	WriteBool(qb422016, b)
-//line views/components/view/Bool.html:9
-	qs422016 := string(qb422016.B)
-//line views/components/view/Bool.html:9
-	qt422016.ReleaseByteBuffer(qb422016)
-//line views/components/view/Bool.html:9
-	return qs422016
-//line views/components/view/Bool.html:9
-}
-
 //line views/components/view/Bool.html:11
-func StreamBoolIcon(qw422016 *qt422016.Writer, b bool, size int, cls string, ps *cutil.PageState) {
+	if b {
+//line views/components/view/Bool.html:11
+		qw422016.N().S(`true`)
+//line views/components/view/Bool.html:11
+	} else {
+//line views/components/view/Bool.html:11
+		qw422016.N().S(`false`)
+//line views/components/view/Bool.html:11
+	}
 //line views/components/view/Bool.html:12
+}
+
+//line views/components/view/Bool.html:12
+func WriteBool(qq422016 qtio422016.Writer, b bool) {
+//line views/components/view/Bool.html:12
+	qw422016 := qt422016.AcquireWriter(qq422016)
+//line views/components/view/Bool.html:12
+	StreamBool(qw422016, b)
+//line views/components/view/Bool.html:12
+	qt422016.ReleaseWriter(qw422016)
+//line views/components/view/Bool.html:12
+}
+
+//line views/components/view/Bool.html:12
+func Bool(b bool) string {
+//line views/components/view/Bool.html:12
+	qb422016 := qt422016.AcquireByteBuffer()
+//line views/components/view/Bool.html:12
+	WriteBool(qb422016, b)
+//line views/components/view/Bool.html:12
+	qs422016 := string(qb422016.B)
+//line views/components/view/Bool.html:12
+	qt422016.ReleaseByteBuffer(qb422016)
+//line views/components/view/Bool.html:12
+	return qs422016
+//line views/components/view/Bool.html:12
+}
+
+//line views/components/view/Bool.html:14
+func StreamBoolIcon(qw422016 *qt422016.Writer, b bool, size int, cls string, ps *cutil.PageState, titles ...string) {
+//line views/components/view/Bool.html:16
 	if cls == "" {
 		cls = "inline"
 	}
-
-//line views/components/view/Bool.html:13
-	if b {
-//line views/components/view/Bool.html:14
-		components.StreamSVGRef(qw422016, "check", size, size, cls, ps)
-//line views/components/view/Bool.html:15
-	} else {
-//line views/components/view/Bool.html:16
-		components.StreamSVGRef(qw422016, "times", size, size, cls, ps)
-//line views/components/view/Bool.html:17
+	extra := ""
+	switch len(titles) {
+	case 0:
+		if b {
+			extra = "true"
+		} else {
+			extra = "false"
+		}
+	case 1:
+		l, r := util.StringSplitLast(titles[0], '|', true)
+		if b || r == "" {
+			extra = l
+		} else {
+			extra = r
+		}
+	case 2:
+		if b {
+			extra = titles[0]
+		} else {
+			extra = titles[1]
+		}
 	}
-//line views/components/view/Bool.html:18
+	if extra != "" {
+		extra = fmt.Sprintf(" title=%q", extra)
+	}
+	icon := "times"
+	if b {
+		icon = "check"
+	}
+
+//line views/components/view/Bool.html:48
+	qw422016.N().S(`<span`)
+//line views/components/view/Bool.html:49
+	qw422016.N().S(extra)
+//line views/components/view/Bool.html:49
+	qw422016.N().S(`>`)
+//line views/components/view/Bool.html:49
+	components.StreamSVGRef(qw422016, icon, size, size, cls, ps)
+//line views/components/view/Bool.html:49
+	qw422016.N().S(`</span>`)
+//line views/components/view/Bool.html:50
 }
 
-//line views/components/view/Bool.html:18
-func WriteBoolIcon(qq422016 qtio422016.Writer, b bool, size int, cls string, ps *cutil.PageState) {
-//line views/components/view/Bool.html:18
+//line views/components/view/Bool.html:50
+func WriteBoolIcon(qq422016 qtio422016.Writer, b bool, size int, cls string, ps *cutil.PageState, titles ...string) {
+//line views/components/view/Bool.html:50
 	qw422016 := qt422016.AcquireWriter(qq422016)
-//line views/components/view/Bool.html:18
-	StreamBoolIcon(qw422016, b, size, cls, ps)
-//line views/components/view/Bool.html:18
+//line views/components/view/Bool.html:50
+	StreamBoolIcon(qw422016, b, size, cls, ps, titles...)
+//line views/components/view/Bool.html:50
 	qt422016.ReleaseWriter(qw422016)
-//line views/components/view/Bool.html:18
+//line views/components/view/Bool.html:50
 }
 
-//line views/components/view/Bool.html:18
-func BoolIcon(b bool, size int, cls string, ps *cutil.PageState) string {
-//line views/components/view/Bool.html:18
+//line views/components/view/Bool.html:50
+func BoolIcon(b bool, size int, cls string, ps *cutil.PageState, titles ...string) string {
+//line views/components/view/Bool.html:50
 	qb422016 := qt422016.AcquireByteBuffer()
-//line views/components/view/Bool.html:18
-	WriteBoolIcon(qb422016, b, size, cls, ps)
-//line views/components/view/Bool.html:18
+//line views/components/view/Bool.html:50
+	WriteBoolIcon(qb422016, b, size, cls, ps, titles...)
+//line views/components/view/Bool.html:50
 	qs422016 := string(qb422016.B)
-//line views/components/view/Bool.html:18
+//line views/components/view/Bool.html:50
 	qt422016.ReleaseByteBuffer(qb422016)
-//line views/components/view/Bool.html:18
+//line views/components/view/Bool.html:50
 	return qs422016
-//line views/components/view/Bool.html:18
+//line views/components/view/Bool.html:50
 }
