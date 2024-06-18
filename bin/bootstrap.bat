ee39a831376a1be4d358b40c3c9014930417dab6
Kyle Unverferth <kyle@kyleu.com>
2024-06-18 13:01:51 -0600

    Dependency upgrade, fix failing build

--- bin/bootstrap.bat
+++ bin/bootstrap.bat
@@ -3,7 +3,7 @@
 rem Downloads and installs the Go libraries and tools needed in other scripts
 
 @ECHO ON
-go install github.com/cosmtrek/air@latest
+go install github.com/air-verse/air@latest
 go install github.com/valyala/quicktemplate/qtc@latest
 go install gotest.tools/gotestsum@latest
 go install mvdan.cc/gofumpt@latest
--- module/core/bin/bootstrap.sh
+++ module/core/bin/bootstrap.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$dir"
 
-go install github.com/cosmtrek/air@latest
+go install github.com/air-verse/air@latest
 go install github.com/valyala/quicktemplate/qtc@latest
 go install gotest.tools/gotestsum@latest
 go install mvdan.cc/gofumpt@latest
