3ebff0afa4c0c82d993ad6a077c982ed7b81c8d6
Kyle Unverferth <kyle@kyleu.com>
2025-09-20 17:01:15 -0600

    v1.7.23

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 var (
-	version = "1.7.22" // updated by bin/tag.sh and ldflags
+	version = "1.7.23" // updated by bin/tag.sh and ldflags
 	commit  = ""
 	date    = "unknown"
 )
--- tools/notarize/notarize.sh
+++ tools/notarize/notarize.sh
@@ -6,7 +6,7 @@ cd "$dir/../.."
 
 if [ "$PUBLISH_TEST" != "true" ]
 then
-  xcrun notarytool submit --apple-id "$APPLE_EMAIL" --team-id "$APPLE_TEAM_ID" --password "$APPLE_PASSWORD" ./build/dist/projectforge_1.7.22_darwin_amd64_desktop.dmg
-  xcrun notarytool submit --apple-id "$APPLE_EMAIL" --team-id "$APPLE_TEAM_ID" --password "$APPLE_PASSWORD" ./build/dist/projectforge_1.7.22_darwin_arm64_desktop.dmg
-  xcrun notarytool submit --apple-id "$APPLE_EMAIL" --team-id "$APPLE_TEAM_ID" --password "$APPLE_PASSWORD" ./build/dist/projectforge_1.7.22_darwin_all_desktop.dmg
+  xcrun notarytool submit --apple-id "$APPLE_EMAIL" --team-id "$APPLE_TEAM_ID" --password "$APPLE_PASSWORD" ./build/dist/projectforge_1.7.23_darwin_amd64_desktop.dmg
+  xcrun notarytool submit --apple-id "$APPLE_EMAIL" --team-id "$APPLE_TEAM_ID" --password "$APPLE_PASSWORD" ./build/dist/projectforge_1.7.23_darwin_arm64_desktop.dmg
+  xcrun notarytool submit --apple-id "$APPLE_EMAIL" --team-id "$APPLE_TEAM_ID" --password "$APPLE_PASSWORD" ./build/dist/projectforge_1.7.23_darwin_all_desktop.dmg
 fi
