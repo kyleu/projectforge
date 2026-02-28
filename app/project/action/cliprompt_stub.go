5080c67c6074e780e2fb07d8a5abcb72bf4197f7
Kyle Unverferth <kyle@kyleu.com>
2026-02-28 13:38:45 -0700

    JS build fix

--- app/project/action/cliprompt_stub.go
+++ app/project/action/cliprompt_stub.go
@@ -5,6 +5,6 @@ package action
 
 import "github.com/pkg/errors"
 
-func promptString(_ string, _ string, _ string) (string, error) {
+func promptString(_ string, _ string, _ string, _ ...bool) (string, error) {
 	return "", errors.New("interactive prompts are unavailable on this platform")
 }
