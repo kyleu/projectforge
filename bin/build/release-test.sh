568a71a7124a5db5f3c50d1ad07a507c95a4bb82
Kyle Unverferth <kyle@kyleu.com>
2026-06-25 17:17:57 -0600

    One last Docker fix, reverting from goreleaser's v2, which still needs baking

--- bin/build/release-test.sh
+++ bin/build/release-test.sh
@@ -26,17 +26,6 @@ require_cmd() {
 
 require_cmd goreleaser "install from https://goreleaser.com/install/"
 
-if grep -q "^dockers_v2:" ./tools/release/.goreleaser.yml; then
-  require_cmd docker "install from https://docs.docker.com/get-docker/"
-
-  export BUILDX_BUILDER="${BUILDX_BUILDER:-projectforge-release}"
-  if ! docker buildx inspect "$BUILDX_BUILDER" >/dev/null 2>&1; then
-    docker buildx create --name "$BUILDX_BUILDER" --driver docker-container --use >/dev/null
-  else
-    docker buildx use "$BUILDX_BUILDER" >/dev/null
-  fi
-  docker buildx inspect --bootstrap "$BUILDX_BUILDER" >/dev/null
-fi
 
 [[ -f "$HOME/bin/oauth" ]] && . "$HOME/bin/oauth"
 
